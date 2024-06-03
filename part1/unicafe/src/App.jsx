8155b5f3d0834312577e6a54a7154face41099a4
aki-cat <orenjiakira@gmail.com>
2024-06-02 22:44:17 -0300

    Exercise 1.8: Done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Button from "./Button";
+import Statistics from "./Statistics";
 
 const App = () => {
   // save clicks of each button to its own state
@@ -7,41 +8,16 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  function calculateTotal() {
-    return good - bad;
-  }
-
-  function calculateAverage() {
-    return (good - bad) / (good + neutral + bad);
-  }
-
-  function calculatePositive() {
-    return `${(good / (good + neutral + bad)) * 100}%`;
-  }
-
   return (
     <div>
       <h1>UniCafé</h1>
-      <h2>Give feedback</h2>
       <div>
+        <h2>Give feedback</h2>
         <Button text='Good' onClick={() => setGood(good + 1)} />
         <Button text='Neutral' onClick={() => setNeutral(neutral + 1)} />
         <Button text='Bad' onClick={() => setBad(bad + 1)} />
       </div>
-
-      <h2>Statistics</h2>
-
-      <div>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-      </div>
-      ---
-      <div>
-        <p>All: {calculateTotal()}</p>
-        <p>Average: {calculateAverage()}</p>
-        <p>Positive: {calculatePositive()}</p>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
---
+++ part1/unicafe/src/Statistics.jsx
@@ -0,0 +1,30 @@
+export default function Statistics({ good, neutral, bad }) {
+  function calculateTotal() {
+    return good - bad;
+  }
+
+  function calculateAverage() {
+    return (good - bad) / (good + neutral + bad);
+  }
+
+  function calculatePositive() {
+    return `${(good / (good + neutral + bad)) * 100}%`;
+  }
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <div>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+      </div>
+      ---
+      <div>
+        <p>All: {calculateTotal()}</p>
+        <p>Average: {calculateAverage()}</p>
+        <p>Positive: {calculatePositive()}</p>
+      </div>
+    </div>
+  );
+}
