3113e143a6999156443fae42c8cee8fee6356545
aki-cat <orenjiakira@gmail.com>
2024-06-02 22:30:16 -0300

    Exercise 1.6: Bootstrap and done

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Button from "./Button";
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>UniCafé</h1>
+
+      <h2>Give feedback</h2>
+
+      <Button text='Good' onClick={() => setGood(good + 1)} />
+      <Button text='Neutral' onClick={() => setNeutral(neutral + 1)} />
+      <Button text='Bad' onClick={() => setBad(bad + 1)} />
+
+      <h2>Statistics</h2>
+
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  );
+};
+
+export default App;
