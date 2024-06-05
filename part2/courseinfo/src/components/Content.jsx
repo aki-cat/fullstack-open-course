7f82b2c22d86b49d636fbb95beb287ecd070a5cb
aki-cat <orenjiakira@gmail.com>
2024-06-05 01:33:06 -0300

    Exercise 2.1: Done

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,11 +1,11 @@
 import Part from "./Part";
 
-export default function Content(props) {
+export default function Content({ parts }) {
   return (
     <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
     </div>
   );
 }
