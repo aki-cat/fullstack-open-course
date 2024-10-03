9264a55837dad40b01302e78e73555f60b5ceac8
aki-cat <928588+aki-cat@users.noreply.github.com>
2024-10-03 14:14:17 -0300

    Exercise 2.9: Done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,16 +3,18 @@ import PhoneNumber from './components/PhoneNumber'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: 1234567890
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newPerson, setNewPerson] = useState({
     name: "",
     number: ""
   });
+
+  const [filter, setFilter] = useState("");
 
   function changeNewName(event) {
     console.log("name", event.target.value);
@@ -46,9 +48,19 @@ const App = () => {
     setPersons(persons.concat([newPerson]));
   }
 
+  function setSearchQuery(event) {
+    setFilter(event.target.value);
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <h2>Lookup</h2>
+      <div>
+        <label for='search'>Find</label>
+        <input name='search' type='text' onChange={setSearchQuery} value={filter} />
+      </div>
+      <h2>New entry</h2>
       <form onSubmit={submitNewPerson}>
         <div>
           <div>
@@ -67,7 +79,9 @@ const App = () => {
       <div>
         <h2>Numbers</h2>
         <ul>
-          {persons.map(person => {
+          {persons.filter(person =>
+            person.name.match(new RegExp(filter, 'i'))
+          ).map(person => {
             return <PhoneNumber key={person.name} name={person.name} number={person.number} />
           })}
         </ul>
